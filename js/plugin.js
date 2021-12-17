da54d2658671a60e380fc7382e575be25db86832
Ahmed Mohamed Ahmed Ragab <36975475+a7med58@users.noreply.github.com>
2021-12-17 15:52:31 +0200

    V1
    Design Used HTML&CSS&JS and Some Library

---
+++ js/plugin.js
@@ -0,0 +1,28 @@
+$(".change-color").click(function() {
+    $(".color-option").fadeToggle();
+});
+
+$('html').niceScroll();
+
+$(document).ready(function() {
+    $('.backtotop').css({ 'display': 'none' });
+    var offset = 200;
+    var duration = 600;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.backtotop').fadeIn(duration);
+        } else {
+            $('.backtotop').fadeOut(duration);
+        }
+    });
+
+    $('.backtotop').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, duration);
+        return false;
+    })
+});
+
+$(window).load(function() {
+    $('.loading, .loading .sk-chase').fadeOut(2000);
+});
