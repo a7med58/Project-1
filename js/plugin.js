a5715de04e2e3c36bfcaeb54f11b170d98f56335
Ahmed Mohamed Ahmed Ragab <36975475+a7med58@users.noreply.github.com>
2021-12-27 23:32:51 +0200

    V3
    Update Loading
    Update JS
    Added Clock
    Add Counter

--- js/plugin.js
+++ js/plugin.js
@@ -78,13 +78,43 @@ var textarea = document.getElementById('text_area'),
     txt_maxlength = textarea.getAttribute('maxlength');
 
 textarea.oninput = function() {
-    'use strict';
-    count.textContent = txt_maxlength - this.value.length;
+        'use strict';
+        count.textContent = txt_maxlength - this.value.length;
 
-    if (count.textContent == 0) {
-        count.classList.add('zero');
-    } else {
-        count.classList.remove('zero');
+        if (count.textContent == 0) {
+            count.classList.add('zero');
+        } else {
+            count.classList.remove('zero');
+        }
+
+    }
+    // Set the date we're counting down to
+var countDownDate = new Date("Dec 31, 2021 12:00:00").getTime(); // CHANGE DATE AND TIME HERE
+
+// Update the count down every 1 second
+var countdown = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the difference between now and the count down date
+    var difference = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    // If the difference is less than 0, stop countdown
+    if (difference < 0) {
+        clearInterval(countdown);
+        days = 0, hours = 0, minutes = 0, seconds = 0;
     }
 
-}
+    // Output the result
+    document.getElementById("days").children[0].innerText = days;
+    document.getElementById("hours").children[0].innerText = hours;
+    document.getElementById("minutes").children[0].innerText = minutes;
+    document.getElementById("seconds").children[0].innerText = seconds;
+}, 1000);
